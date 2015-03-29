d7ef500d43ab45549b0e5b0c64d158879b739c4f
GyulaHarangi <gyulaharangi@gmail.com>
2015-03-29 12:11:42 +0200

    Framework osztály létrehozása

---
+++ Class/Framework.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gyula
+ * Date: 2015.03.29.
+ * Time: 12:08
+ */
+
+class Framework {
+    public function __construct()
+    {
+        echo"<html>";
+        echo"<head>";
+        echo"</head>";
+        echo"<body>";
+            echo"teszt";
+        echo"</body>";
+        echo"</html>";
+    }
+}
